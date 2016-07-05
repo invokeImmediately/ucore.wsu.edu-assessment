01fb9297946793e3d7ebb4eac432faa102b4a168
Daniel Rieck <danielcrieck@gmail.com>
2016-07-05 16:58:16 -0700

    Updating build
    Rebuilt production files to incorporate recent changes to source code.

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -1,140 +1,3 @@
-// See [https://github.com/invokeImmediately/distinguishedscholarships.wsu.edu] for repository of source code
-/************************************************************************************************************
- * CUSTOM JQUERY-BASED DYNAMIC CONTENT                                                                      *
- ************************************************************************************************************/
-(function ($) {
-	$(document).ready(function () {
-		/**********************************************************************************************
-		 * Tweak HTML source to work around some quirks of WordPress setup                            *
-		 **********************************************************************************************/
-		var siteURL = window.location.pathname;
-		switch (siteURL) {
-			/* case '/':
-			$('#menu-item-35').remove();
-			$('#spine-sitenav ul li').first().css('border-top', 'none');
-			$('#spine-sitenav').addClass('homeless');
-			break;*/
-		case '/news/':
-			$('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
-			break;
-		}
-		
-		initNiloaPortal(".niloa-portal");
-	});
-	
-	function HexagonalButton($fromElem) {
-		var validArg = isJQuery($fromElem);
-		var tan30 = Math.tan(30 / 180 * Math.PI);
-		this.origin = {
-			x: validArg ? $fromElem.offset().left : undefined,
-			y: validArg ? $fromElem.offset().top : undefined
-		};
-		this.apotherm = $fromElem.width() / 2;
-		this.vertices = [
-			{
-				x: validArg ? (this.origin.x + this.apotherm) : undefined,
-				y: validArg ? this.origin.y : undefined
-			},
-			{
-				x: validArg ? (this.origin.x + this.apotherm * 2) : undefined,
-				y: validArg ? (this.origin.y + tan30 * this.apotherm) : undefined
-			},
-			{
-				x: validArg ? (this.origin.x + this.apotherm * 2) : undefined,
-				y: validArg ? (this.origin.y + 3 * tan30 * this.apotherm) : undefined
-			},
-			{
-				x: validArg ? (this.origin.x + this.apotherm) : undefined,
-				y: validArg ? (this.origin.y + 4 * tan30 * this.apotherm) : undefined
-			},
-			{
-				x: validArg ? this.origin.x : undefined,
-				y: validArg ? (this.origin.y + 3 * tan30 * this.apotherm) : undefined
-			},
-			{
-				x: validArg ? this.origin.x : undefined,
-				y: validArg ? (this.origin.y + tan30 * this.apotherm) : undefined
-			}
-		];
-		this.keySegments = [
-			{
-				slope: validArg ? -tan30 : undefined,
-				intercept: validArg ? this.vertices[5].y : undefined
-			},
-			{
-				slope: validArg ? tan30 : undefined,
-				intercept: validArg ? this.vertices[4].y : undefined
-			},
-			{
-				slope: validArg ? tan30 : undefined,
-				intercept: validArg ? this.vertices[0].y : undefined
-			},
-			{
-				slope: validArg ? -tan30 : undefined,
-				intercept: validArg ? this.vertices[3].y : undefined
-			}
-		];
-	}
-
-	HexagonalButton.prototype.getSegmentY(whichSgmnt, pageX) {
-		var pageY = undefined;
-		if(whichSgmnt >= 0 && whichSgmnt <=3) {
-			pageY = this.keySegments[whichSgmnt].slope * pageX + this.keySegments[whichSgmnt].intercept;
-		}
-		return pageY;
-	}
-	
-	HexagonalButton.prototype.isWithinArea(pageX, pageY) {
-		var result = false;
-		if(pageX <= this.vertices[0].x) {
-			var sgmnt0Y = this.getSegmentY(0, pageX);
-			var sgmnt1Y = this.getSegmentY(1, pageX);
-			result = pageY >= sgmnt0Y && pageY <= sgmnt1Y;
-		}
-		else {
-			var sgmnt2Y = this.getSegmentY(2, pageX);
-			var sgmnt3Y = this.getSegmentY(3, pageX);
-			result = pageY >= sgmnt2Y && pageY <= sgmnt3Y;
-		}
-		return result;
-	}
-	
-	function handleNiloaMouseMove($whichBttn, bttnCoords, moveEvent) {
-		if(isJQuery($whichBttn) && bttnCoords && Object.getPrototypeOf(bttnCoords) === HexagonalButton) {
-			if(bttnCoords.isWithinArea(moveEvent.pageX, moveEvent.pageY)) {
-				$whichBttn.addClass("hovered");
-			}
-			else {
-				$whichBttn.removeClass("hovered");
-			}
-		}
-	}
-	
-	function handleNiloaClick($whichBttn, bttnCoords, clickEvent) {
-		if(isJQuery($whichBttn) && bttnCoords && Object.getPrototypeOf(bttnCoords) === HexagonalButton) {
-			if(bttnCoords.isWithinArea(moveEvent.pageX, moveEvent.pageY)) {
-				alert("Clicked!");
-			}
-		}
-	}
-
-	function initNiloaPortal(slctrPortal) {
-		var $portal = $(slctrPortal);
-		if($portal.length == 1) {
-			var $bttns = $portal.find("li.button");
-			$bttns.each(function () {
-				var $thisBttn = $(this);
-				$thisBttn.mousemove(function(moveEvent) {
-					var hxgnlBttn = new HexagonalButton($(this));
-					handleNiloaMouseMove($thisBttn, hxgnlBttn, moveEvent);
-				}).click(function(clickEvent) {
-					var hxgnlBttn = new HexagonalButton($(this));
-					handleNiloaClick($thisBttn, hxgnlBttn, clickEvent);
-				});
-			});
-		}
-	}
-})(jQuery);
 /**********************************************************************************************************************
  CUSTOM JQUERY-BASED DYNAMIC CONTENT
  *********************************************************************************************************************/
@@ -330,6 +193,143 @@ function isJQuery($obj) {
     function resizeLrgFrmtSideRight(slctrSideRight, slctrColOne, slctrColTwo, trggrWidth, animDuration) {
         finalizeLrgFrmtSideRight(slctrSideRight, slctrColOne, slctrColTwo, trggrWidth, animDuration);
     }
+})(jQuery);
+// See [https://github.com/invokeImmediately/distinguishedscholarships.wsu.edu] for repository of source code
+/************************************************************************************************************
+ * CUSTOM JQUERY-BASED DYNAMIC CONTENT                                                                      *
+ ************************************************************************************************************/
+(function ($) {
+	$(document).ready(function () {
+		/**********************************************************************************************
+		 * Tweak HTML source to work around some quirks of WordPress setup                            *
+		 **********************************************************************************************/
+		var siteURL = window.location.pathname;
+		switch (siteURL) {
+			/* case '/':
+			$('#menu-item-35').remove();
+			$('#spine-sitenav ul li').first().css('border-top', 'none');
+			$('#spine-sitenav').addClass('homeless');
+			break;*/
+		case '/news/':
+			$('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
+			break;
+		}
+		
+		initNiloaPortal(".niloa-portal");
+	});
+	
+	function HexagonalButton($fromElem) {
+		var validArg = isJQuery($fromElem);
+		var tan30 = Math.tan(30 / 180 * Math.PI);
+		this.origin = {
+			x: validArg ? $fromElem.offset().left : undefined,
+			y: validArg ? $fromElem.offset().top : undefined
+		};
+		this.apotherm = $fromElem.width() / 2;
+		this.vertices = [
+			{
+				x: validArg ? (this.origin.x + this.apotherm) : undefined,
+				y: validArg ? this.origin.y : undefined
+			},
+			{
+				x: validArg ? (this.origin.x + this.apotherm * 2) : undefined,
+				y: validArg ? (this.origin.y + tan30 * this.apotherm) : undefined
+			},
+			{
+				x: validArg ? (this.origin.x + this.apotherm * 2) : undefined,
+				y: validArg ? (this.origin.y + 3 * tan30 * this.apotherm) : undefined
+			},
+			{
+				x: validArg ? (this.origin.x + this.apotherm) : undefined,
+				y: validArg ? (this.origin.y + 4 * tan30 * this.apotherm) : undefined
+			},
+			{
+				x: validArg ? this.origin.x : undefined,
+				y: validArg ? (this.origin.y + 3 * tan30 * this.apotherm) : undefined
+			},
+			{
+				x: validArg ? this.origin.x : undefined,
+				y: validArg ? (this.origin.y + tan30 * this.apotherm) : undefined
+			}
+		];
+		this.keySegments = [
+			{
+				slope: validArg ? -tan30 : undefined,
+				intercept: validArg ? this.vertices[5].y : undefined
+			},
+			{
+				slope: validArg ? tan30 : undefined,
+				intercept: validArg ? this.vertices[4].y : undefined
+			},
+			{
+				slope: validArg ? tan30 : undefined,
+				intercept: validArg ? this.vertices[0].y : undefined
+			},
+			{
+				slope: validArg ? -tan30 : undefined,
+				intercept: validArg ? this.vertices[3].y : undefined
+			}
+		];
+	}
+
+	HexagonalButton.prototype.getSegmentY = function(whichSgmnt, pageX) {
+		var pageY = undefined;
+		if(whichSgmnt >= 0 && whichSgmnt <=3) {
+			pageY = this.keySegments[whichSgmnt].slope * pageX + this.keySegments[whichSgmnt].intercept;
+		}
+		return pageY;
+	}
+	
+	HexagonalButton.prototype.isWithinArea = function(pageX, pageY) {
+		var result = false;
+		if(pageX <= this.vertices[0].x) {
+			var sgmnt0Y = this.getSegmentY(0, pageX);
+			var sgmnt1Y = this.getSegmentY(1, pageX);
+			result = pageY >= sgmnt0Y && pageY <= sgmnt1Y;
+		}
+		else {
+			var sgmnt2Y = this.getSegmentY(2, pageX);
+			var sgmnt3Y = this.getSegmentY(3, pageX);
+			result = pageY >= sgmnt2Y && pageY <= sgmnt3Y;
+		}
+		return result;
+	}
+	
+	function handleNiloaMouseMove($whichBttn, bttnCoords, moveEvent) {
+		if(isJQuery($whichBttn) && bttnCoords && Object.getPrototypeOf(bttnCoords) === HexagonalButton) {
+			if(bttnCoords.isWithinArea(moveEvent.pageX, moveEvent.pageY)) {
+				$whichBttn.addClass("hovered");
+			}
+			else {
+				$whichBttn.removeClass("hovered");
+			}
+		}
+	}
+	
+	function handleNiloaClick($whichBttn, bttnCoords, clickEvent) {
+		if(isJQuery($whichBttn) && bttnCoords && Object.getPrototypeOf(bttnCoords) === HexagonalButton) {
+			if(bttnCoords.isWithinArea(moveEvent.pageX, moveEvent.pageY)) {
+				alert("Clicked!");
+			}
+		}
+	}
+
+	function initNiloaPortal(slctrPortal) {
+		var $portal = $(slctrPortal);
+		if($portal.length == 1) {
+			var $bttns = $portal.find("li.button");
+			$bttns.each(function () {
+				var $thisBttn = $(this);
+				$thisBttn.mousemove(function(moveEvent) {
+					var hxgnlBttn = new HexagonalButton($(this));
+					handleNiloaMouseMove($thisBttn, hxgnlBttn, moveEvent);
+				}).click(function(clickEvent) {
+					var hxgnlBttn = new HexagonalButton($(this));
+					handleNiloaClick($thisBttn, hxgnlBttn, clickEvent);
+				});
+			});
+		}
+	}
 })(jQuery);
 /**********************************************************************************************************************
  JQUERY QTIP TOOL TIPS PLUGIN
