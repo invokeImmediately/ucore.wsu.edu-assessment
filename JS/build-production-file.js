ba5764b33007d9e6b532648f88255a427e8025d0
Daniel Rieck <danielcrieck@gmail.com>
2016-07-11 10:39:35 -0700

    Changing order of build dependency loading
    Switched the OUE-wide custom JS file to load first.

--- JS/build-production-file.js
+++ JS/build-production-file.js
@@ -1,8 +1,8 @@
 /* NODE.JS - Build Production JavaScript File */
 var concat = require('../../../../AppData/Roaming/npm/node_modules/concat-files');
 concat([
- './ucore-assessment-custom.js',
  '../WSU-UE---JS/jQuery.oue-custom.js',
+ './ucore-assessment-custom.js',
  '../WSU-UE---JS/jQuery.qTip.js',
  '../WSU-UE---JS/jQuery.textResize.js',
  '../WSU-UE---JS/jQuery.masonry.min.js'
